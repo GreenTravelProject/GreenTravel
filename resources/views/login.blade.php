cfb0a42bbd49302c4a5ad1105bf2b2b6de032bec
neumanv <guillermoanv25@gmail.com>
2023-02-06 14:07:16 +0100

    Admin, login y sgnup terminados

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,10 +2,12 @@
 @section('login')
     <div id="login">
         <form action="post" class="text-center">
+            <h1>INICIAR SESIÓN</h1><br>
             <h1><i class="bi bi-person-circle fs-1"></i></h1>
             <input class="form-control" type="text" name="name" placeholder="Email o user" required><br>
             <input class="form-control" type="password" name="password" placeholder="Contraseña" required><br>
-            <input class="bg-success btn" type="submit">
+            <button class="bg-success btn w-50 text-light" type="submit">Iniciar sesión</button>
         </form>
     </div>
+    <p id="p-login" class="text-center">Si no estás registrado pulsa <a href="{{ route('signup') }}">aquí</a></p>
 @endsection
