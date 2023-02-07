8e5e33d217d20feafe45d2b2a334d7ae93731f88
neumanv <guillermoanv25@gmail.com>
2023-02-07 10:31:31 +0100

    Responsive index y signup

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
         <form action="post" class="text-center">
             <h1>INICIAR SESIÓN</h1><br>
             <h1><i class="bi bi-person-circle fs-1"></i></h1>
-            <input class="form-control" type="text" name="name" placeholder="Email o user" required><br>
+            <input class="form-control" type="text" name="name" placeholder="Email o user" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required><br>
             <input class="form-control" type="password" name="password" placeholder="Contraseña" required><br>
             <button class="bg-success btn w-50 text-light" type="submit">Iniciar sesión</button>
         </form>
