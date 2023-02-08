a676241e57339748414a853e66ccd2aabe01a8a5
neumanv <guillermoanv25@gmail.com>
2023-02-08 12:25:13 +0100

    Usuario corregido

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,34 +3,27 @@
 @section('user')
     <div class="menu">
         <aside>
-            <h1 class="text-center p-5">MENÚ</h1><hr>
-            <div class="container p-3">
-                <h3 class="m-0">hola</h3>
-            <h1 id="title-user" class="text-center">MENÚ</h1><hr>
-            <div class="apartado">
-                <h4>Datos</h4>
-            </div><hr>
-            <div class="container p-3">
-                <h3 class="m-0">hola</h3>
-            <div class="apartado">
-                <h4>Favoritos</h4>
-            </div><hr>
-            <div class="container p-3">
-                <h3 class="m-0">hola</h3>
-            <div class="apartado">
-                <h4>Cambio</h4>
-            </div><hr>
-            <div class="container p-3">
-                <h3 class="m-0">hola</h3>
-            <div class="apartado">
-                <h4>Carrito</h4>
-            </div><hr class="mb-0">
+            <div class="text-center">
+                <h1 class="text-center mt-3">MENÚ</h1><hr>
+                <div class="apartado">
+                    <h4>Datos</h4>
+                </div><hr>
+                <div class="apartado">
+                    <h4>Favoritos</h4>
+                </div><hr>
+                <div class="apartado">
+                    <h4>Cambio</h4>
+                </div><hr>
+                <div class="apartado">
+                    <h4>Carrito</h4>
+                </div><hr class="mb-0">
+            </div>
         </aside>
         <article>
-            sd
-            sdos diseñadores web son expertos técnicos con un talento creativo para diseñar y mejorar los sitios web. Una buena carta de presentación de un diseñador web debe mostrar estos atributos destacando los conocimientos y la experiencia del candidato en el puesto, con especial énfasis en su capacidad para conceptualizar ideas creativas y cumplir los plazos.
-
-            No pierdas más el tiempo y aprende ya a crear una carta de presentación de diseñador web sólida que te ayude a destacar frente al resto de candidatos. Tenemos ejemplos y guías paso a paso. ¡Vamos a por ello!
+            <p class="p-3">sdos diseñadores web son expertos técnicos con un talento creativo para diseñar y mejorar los sitios web. Una
+            buena carta de presentación de un diseñador web debe mostrar estos atributos destacando los conocimientos y la
+            experiencia del candidato en el puesto, con especial énfasis en su capacidad para conceptualizar ideas creativas
+            y cumplir los plazos.</p> 
 
         </article>
     </div>
