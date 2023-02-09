66c990640b4ff1a6d5d47242a16dcd48f07d9f24
Laura Valiente <lauravcruzdaw@gmail.com>
2023-02-09 18:38:13 +0100

    descargando main

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\LogInController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
