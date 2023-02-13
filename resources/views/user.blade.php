19dbb3436a58c877c8dc2ad26bcc5ac400adcd44
neumanv <guillermoanv25@gmail.com>
2023-02-13 12:01:59 +0100

    Editando datos

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,19 +3,19 @@
 @section('user')
     <section id="user-cont">
         <div class="row m-0 w-100" id="full-page">
-            <div class="col-md-2 col-sm-2 p-0">
+            <div class="col-md-2 col-sm-2 p-0 bg-light">
                 <div class="profile-sidebar">
                     <div class="profile-user">
                         <div class="profile-name text-center">
                             <h1 class="p-4">MENÚ</h1><hr class="m-0">
                         </div>
                     </div>
-                    <nav class="profile-menu mt-0">
+                    <div class="profile-menu mt-0">
                         <h4 class="p-4" onclick="reveal('user-info1');">Datos</h4><hr class="m-0">
                         <h4 class="p-4" onclick="reveal('user-info2');">Favoritos</h4><hr class="m-0">
                         <h4 class="p-4" onclick="reveal('user-info3');">Cambio contraseña</h4><hr class="m-0">
                         <h4 class="p-4" onclick="reveal('user-info4');">Carrito</h4>
-                    </nav>
+                    </div>
                 </div>
             </div>
             <div id="user-data" class="col-md-10 col-sm-2 pb-0">
@@ -25,44 +25,9 @@
                         <div class="container p-4">
                             <form method="post">
                                 @csrf
-                                <div class="mt-2">
-                                    @error('nombre')
-                                        <div class="alert alert-danger">
-                                            El formato del nombre no es válido
-                                        </div>
-                                    @enderror
-                                    @error('surname')
-                                        <div class="alert alert-danger">
-                                            El formato del apellido no es válido
-                                        </div>
-                                    @enderror
-                                    @error('phone')
-                                        <div class="alert alert-danger">
-                                            El teléfono no cumple con el formato requerido
-                                        </div>
-                                    @enderror
-                                    @error('birth_day')
-                                        <div class="alert alert-danger">
-                                            La fecha introducida no es válida
-                                        </div>
-                                    @enderror
-                                    @error('genre')
-                                        <div class="alert alert-danger">
-                                            Debes seleccionar un género
-                                        </div>
-                                    @enderror
-                                    @error('email')
-                                        <div class="alert alert-danger">
-                                            La dirección de correo no es válida
-                                        </div>
-                                    @enderror
-                                    @if (session('mensaje'))
-                                        <div class="alert alert-success">{{ session('mensaje') }}</div>
-                                    @endif
-                                </div>
                                 <div class="row m-3">
                                     <div class="col-md-6 col-sm-12">
-                                        <label>Nombre</label><input class="form-control" type="text" name="name" placeholder="Mínimo 3 caracteres" value="{{ old('name') }}" required><br>
+                                        <label>Nombre</label><input class="form-control" type="text" name="name" placeholder="Mínimo 3 caracteres" value="{{$usuario->name}}" required><br>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
                                         <label>Apellidos</label><input class="form-control" type="text" name="surname" placeholder="Mínimo 3 caracteres" value="{{ old('surname') }}" required><br>
@@ -71,8 +36,7 @@
                                         <label>Número de teléfono</label><input class="form-control" type="tel" name="phone" placeholder="*" value="{{ old('phone') }}" required><br>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
-                                        <label>Género</label><select class="form-control" type="text" name="genre"
-                                            value>
+                                        <label>Género</label><select class="form-control" type="text" name="genre" value>
                                             <option selected="selected" disabled>-</option>
                                             <option value="M">Masculino</option>
                                             <option value="F">Femenino</option>
@@ -80,7 +44,7 @@
                                         </select>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
-                                        <label>Correo electrónico</label>
+                                        <label id="nacimiento">Correo electrónico</label>
                                         <div class="input-group">
                                             <div class="input-group-text">@</div><input class="form-control" type="email" name="email" placeholder="*" value="{{ old('email') }}" required>
                                         </div>
@@ -88,9 +52,69 @@
                                     <div class="col-md-6 col-sm-12">
                                         <label id="nacimiento">Fecha de nacimiento*</label><input class="form-control" type="date" name="birth_date" required><br>
                                     </div>
-                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                        <button id="btn-signup" class="bg-success btn mb-5 text-light text-center" type="submit">Cambiar</button>
+                                </div><hr>
+                                <div>
+                                    <div class="row m-3">
+                                        <div class="col-md-6 col-sm-12">
+                                            <label>País</label><select class="form-control" type="text" name="country" required><br>
+                                                <option disabled selected>-</option>
+                                                <option value="DE">Alemania</option>
+                                                <option value="AD">Andorra</option>
+                                                <option value="AM">Armenia</option>
+                                                <option value="AT">Austria</option>
+                                                <option value="BE">Bélgica</option>
+                                                <option value="BY">Bielorrusia</option>
+                                                <option value="BA">Bosnia</option>
+                                                <option value="BG">Bulgaria</option>
+                                                <option value="CY">Chipre</option>
+                                                <option value="HR">Croacia</option>
+                                                <option value="DK">Dinamarca</option>
+                                                <option value="SI">Eslovenia</option>
+                                                <option value="ES">España</option>
+                                                <option value="EE">Estonia</option>
+                                                <option value="FI">Finlandia</option>
+                                                <option value="FR">Francia</option>
+                                                <option value="GE">Georgia</option>
+                                                <option value="GI">Gibraltar</option>
+                                                <option value="GR">Grecia</option>
+                                                <option value="HU">Hungría</option>
+                                                <option value="IE">Irlanda</option>
+                                                <option value="IS">Islandia</option>
+                                                <option value="FO">Islas Feroe</option>
+                                                <option value="IT">Italia</option>
+                                                <option value="LV">Letonia</option>
+                                                <option value="LI">Liechtenstein</option>
+                                                <option value="LT">Lituania</option>
+                                                <option value="LU">Luxemburgo</option>
+                                                <option value="MK">Macedonia</option>
+                                                <option value="MT">Malta</option>
+                                                <option value="MD">Moldavia</option>
+                                                <option value="MC">Mónaco</option>
+                                                <option value="NO">Noruega</option>
+                                                <option value="NL">Países Bajos</option>
+                                                <option value="PL">Polonia</option>
+                                                <option value="PT">Portugal</option>
+                                                <option value="UK">Reino Unido</option>
+                                                <option value="CZ">República Checa</option>
+                                                <option value="SK">Eslovaquia</option>
+                                                <option value="RO">Rumania</option>
+                                                <option value="RU">Rusia</option>
+                                                <option value="SM">San Marino</option>
+                                                <option value="SE">Suecia</option>
+                                                <option value="CH">Suiza</option>
+                                                <option value="UA">Ucrania</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-6 col-sm-12">
+                                            <label>Localidad</label><input class="form-control" type="text" name="city" placeholder="*" required><br>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <label>Correo electrónico</label><input class="form-control" type="text" name="street" placeholder="Dirección exacta" value="{{old('email')}}" required></div><br>
+                                        </div>
                                     </div>
+                                </div>
+                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                    <button id="btn-signup" class="bg-success btn mb-5 text-light text-center" type="submit">Cambiar</button>
                                 </div>
                             </form>
                         </div>
@@ -141,33 +165,11 @@
                     <div class="container">
                         <h1>CARRITO</h1>
                         <div class="container p-4">
-                            <div class="col-md-3 mb-3">
-                                <div class="card">
-                                    <img class="img-fluid" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
-                                    <div class="card-body">
-                                        <h4 class="card-title">Special title treatment</h4>
-                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </span>
             </div>
         </div>
     </section>
-    <script>
-        function reveal(id) {
-            var e = document.getElementById(id);
-            if (e.style.display == 'inline') {
-                e.style.display = 'none';
-            } else {
-                var allTexts = document.querySelectorAll(".user-info");
-                for (var i = 0, len = allTexts.length; i < len; i++) {
-                    allTexts[i].style.display = 'none';
-                }
-                e.style.display = 'inline';
-            }
-        }
-    </script>
 @endsection
