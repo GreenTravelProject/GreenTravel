58cbc1cee8d2bd7e4315a0e6474c5fdb2e8407f7
lauravcruz <lauravcruzdaw@gmail.com>
2023-02-13 13:34:56 +0100

    pull

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>Green Travel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/custom.css'])
-    <script defer src="assets/js/custom.js"></script>
+    <script defer src="{{ URL::asset("assets/js/custom.js")}}"></script>
     <link rel="icon" type="image/jpg" href="{{ URL::asset('favicon.ico') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
