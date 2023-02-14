8979fb22835478b1e57beb174bbd814b6a07d0cf
neumanv <guillermoanv25@gmail.com>
2023-02-14 10:14:49 +0100

    Cambios

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -191,22 +191,4 @@
         </div>
         </div>
     </section>
-    <script>
-        let expanded = false;
-
-function reveal(id) {
-    let e = document.getElementById(id);
-
-    if(e.style.display == "inline"){
-        e.style.display = "none";
-    }else{
-        let allTexts = document.querySelectorAll(".user-info");
-        for (let i = 0, len = allTexts.length; i < len; i++) {
-            allTexts[i].style.display = "none";
-        }
-        
-        e.style.display = "inline";
-    }
-}
-    </script>
 @endsection
