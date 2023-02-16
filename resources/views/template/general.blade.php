7ab85e394d4ac8e410f76df8f81ee45c98bd0b26
lauravcruz <lauravcruzdaw@gmail.com>
2023-02-16 13:48:21 +0100

    migrations perfect

--- resources/views/template/general.blade.php
+++ resources/views/template/general.blade.php
@@ -22,22 +22,25 @@
                     <a class="navbar-brand" href="{{ route('index') }}"><img src="{{ URL::asset('img/LOGO_NAV.png') }}"></a>
                     <ul class="navbar-nav">
                         @foreach ($categories as $category)
-                            <li class="nav-item"><a class="nav-link" href="{{ route('category', $category->id) }}">{{ $category->name }}</a></li>
+                            <li class="nav-item"><a class="nav-link"
+                                    href="{{ route('category', $category->id) }}">{{ $category->name }}</a></li>
                         @endforeach
                     </ul>
                 </ul>
             </div>
-            <div class="collapse navbar-collapse  d-flex justify-content-end" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav">
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}"><i class="bi bi-person-fill"></i> {{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('login') }}"><i class="bi bi-person-fill"></i>
+                                {{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}"><i class="bi bi-person-fill-add"></i> {{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}"><i class="bi bi-person-fill-add"></i>
+                                    {{ __('Register') }}</a>
                             </li>
                         @endif
                     @else
@@ -105,7 +108,8 @@
                     <div class="col-md-3 col-12 my-sm-0 mt-5">
                         <ul class="list-unstyled">
                             @foreach ($categories as $category)
-                                <li class="mt-md-1"><a class="text-white" href="{{ route('category', $category->id) }}">{{ $category->name }}</a></li>
+                                <li class="mt-md-1"><a class="text-white"
+                                        href="{{ route('category', $category->id) }}">{{ $category->name }}</a></li>
                             @endforeach
                         </ul>
                     </div>
@@ -129,5 +133,5 @@
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
 @endsection
