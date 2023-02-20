40b856b150afb99069af273b9ec66afacdcf0b49
Jairo <jgtrue@protonmail.com>
2023-02-20 19:13:01 +0100

    favorite working

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -15,17 +15,16 @@ class FavoriteController extends Controller
         $products = $favorite->products;
         return view('', @compact("products"));
     }
-    // TODO: Termina consulta en tabla pivote favorite_product
+
     public function add(Request $request)
     {
         $favorite = Favorite::where('user_id', Auth::id())->first();
-        // $favorite->products->where('product_id', $request)->exists();
 
-        if(DB::table('favorite_product')->where('product_id', $request->product && 'favorite_id', $favorite->id)->doesntExist()){
+        if(DB::table('favorite_product')->where('product_id', $request->product)->where('favorite_id', $favorite->id)->doesntExist()){
             $favorite->products()->attach($request->product);
             return back()->with('mensaje', 'El producto se ha añadido a favoritos');
 
-        }if(DB::table('favorite_product')->where('product_id', $request->product && 'favorite_id', $favorite->id)->exists()){
+        }if(DB::table('favorite_product')->where('product_id', $request->product)->where('favorite_id', $favorite->id)->exists()){
             $favorite->products()->detach($request->product);
             return back()->with('mensaje', 'El producto se ha eliminado de favoritos');
         }
