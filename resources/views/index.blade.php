0dc0cdd2b461797de4a5971824279ecb90f6e86f
neumanv <guillermoanv25@gmail.com>
2023-02-21 14:08:38 +0100

    Cambios estéticos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
                                             <!-- Sale badge-->
                                             <div class="badge bg-dark text-white position-absolute"
                                                 style="top: 0.5rem; right: 0.5rem">
-                                                {{ $products[$i]->stock}}
+                                                <i class="bi bi-person-fill">{{ $products[$i]->stock}}</i>
                                             </div>
                                             <!-- Product image-->
                                             <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
