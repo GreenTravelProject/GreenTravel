d0a3dd14d2fad177943ceef336ae28190254b1ef
neumanv <guillermoanv25@gmail.com>
2023-02-22 20:24:28 +0100

    Ejecutar script con php artisan migrate:fresh --seed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         //TODO: DOCUMENTACIÓN. controlar que las categorías de la base de datos existen: (necesitamos un seeder con las categorías)
-        view()->share('categories', Category::all());
+        //view()->share('categories', Category::all());
     }
 }
